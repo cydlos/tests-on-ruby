cde90bc140d86d3b92bf6e46dbef21b433c93d32
cyd losekann <cydnel@gmail.com>
2022-09-16 11:52:31 -0300

    comments added

--- team_spec.rb
+++ team_spec.rb
@@ -2,16 +2,19 @@ require 'team.rb'
 
 RSpec.describe Team do
 
+  # check whether there is a 'Team' class
   it 'creates a team class' do
     team = Team.new
     expect(team).to be_kind_of(Team)
   end
 
+  # checks whether 'play' method exists:
   it 'plays' do
     team = Team.new
     expect(team).to respond_to(:play)
   end
 
+  # checks whether 'play'returns the correct message.
   it 'responds to play with a message' do
     team = Team.new
     expect(team.play).to eq ('We are playing')
